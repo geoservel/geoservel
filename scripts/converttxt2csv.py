c60972df89302b80921762db38682194279bde53
Eduardo Ramírez <ejramire@uc.cl>
2014-02-27 09:48:19 -0300

    readfiles

--- scripts/converttxt2csv.py
+++ scripts/converttxt2csv.py
@@ -1,1 +1,31 @@
-#convert txt files in consistent csv files
+#convert txt files to consistent csv files
+import glob
+
+class Votante:
+    def __init__(self):
+        self.nombre = ''
+        self.rut = 0
+        self.dv = ''
+        self.sexo = ''
+        self.direccion = ''
+        self.circunscripcion = ''
+        self.mesa = ''
+        self.region = ''
+        self.comuna = ''
+
+    def __str__(self):
+        return '%s;%i;%s;%s;%s;%s;%s;%s;%s' % (self.nombre, self.rut , self.dv, self.sexo, self.direccion, self.circunscripcion, self.mesa, self.region, self.comuna)
+
+
+#for file in txt folder
+for filepath in glob.glob('txts/txtsample/*.txt'):
+    f = open(filepath)
+    for line in f:
+        print line
+
+
+
+
+
+
+
